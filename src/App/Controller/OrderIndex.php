3d5768f180206a6c36c2e09ff6757f2c83bd17c6
mohammad <mohammad@mohammad.mb.bashirinia@gmail.com>
2025-03-18 15:59:05 +0330

    add submit order endpoint

--- src/App/Controller/OrderIndex.php
+++ src/App/Controller/OrderIndex.php
@@ -7,10 +7,14 @@ namespace App\Controller;
 use App\Repo\OrdersRepo;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use Valitron\Validator;
 
 class OrderIndex
 {
-    public function __construct(private OrdersRepo $orderR) {}
+    public function __construct(
+        private OrdersRepo $orderR,
+        private Validator $validator
+    ) {}
 
     public function AllOrders(Request $req, Response $res): Response
     {
@@ -30,6 +34,44 @@ class OrderIndex
 
         $body = json_encode($data);
         $res->getBody()->write($body);
+
+        return $res;
+    }
+
+    public function submitOrder(Request $req, Response $res): Response
+    {
+        $user = $req->getAttribute("user");
+        $user_id = $user->user_id;
+
+        $body = $req->getParsedBody();
+
+        $this->validator->mapFieldsRules([
+            "cart_id" => ['required', 'integer', ['min', 1]],
+        ]);
+        $this->validator = $this->validator->withData($body);
+
+        if (!$this->validator->validate()) {
+            $res->getBody()->write(json_encode($this->validator->errors()));
+            return $res->withStatus(422);
+        }
+
+        $cart_id = $body["cart_id"];
+
+        $data = $this->orderR->createOrder($user_id, (int)$cart_id);
+
+        if ($data === false) {
+            $temp = json_encode([
+                "message" => " order not created!",
+            ]);
+            $res = $res->withStatus(404);
+        } else {
+            $temp = json_encode([
+                "message" => " order submitted",
+                "id" => $data
+            ]);
+            $res = $res->withStatus(201);
+        }
+        $res->getBody()->write($temp);
 
         return $res;
     }
--- src/App/Repo/OrdersRepo.php
+++ src/App/Repo/OrdersRepo.php
@@ -11,15 +11,6 @@ class OrdersRepo
 {
     public function __construct(private Database $db) {}
 
-    // public function getAll(): array
-    // {
-    //     $pdo = $this->db->getConn();
-
-    //     $stmt = $pdo->query("SELECT * FROM orders");
-    //     $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    //     return $data;
-    // }
-
     public function getAll(int $userId): array
     {
 
@@ -49,5 +40,95 @@ class OrdersRepo
         $stmt->execute();
 
         return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function createOrder(int $userId, int $cartId): string|bool
+    {
+        $pdo = $this->db->getConn();
+
+        $sql1 = "SELECT * FROM cart WHERE id = :cart_id  AND user_id = :user_id AND is_active = TRUE";
+
+        $stmt = $pdo->prepare($sql1);
+        $stmt->bindValue("cart_id", $cartId, PDO::PARAM_INT);
+        $stmt->bindValue("user_id", $userId, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $res1 = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($res1 === false) {
+            echo "1111";
+            return false;
+        }
+
+        $sql2 = "UPDATE cart SET is_active = FALSE WHERE id = :cart_id";
+
+        $stmt = $pdo->prepare($sql2);
+        $stmt->bindValue("cart_id", $cartId, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $res2 = $stmt->rowCount();
+        if ($res2 !== 1) {
+            echo "2222";
+
+            return false;
+        }
+
+        $sql3 = "SELECT * FROM cart_items WHERE cart_id = :cart_id";
+
+        $stmt = $pdo->prepare($sql3);
+        $stmt->bindValue("cart_id", $cartId, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $res3 = $stmt->fetchAll();
+        if (empty($res3)) {
+            echo "333";
+
+            return false;
+        }
+
+        foreach ($res3 as $key => $value) {
+            $sql4 = "SELECT * FROM products WHERE id = :product_id";
+
+            $stmt = $pdo->prepare($sql4);
+            $stmt->bindValue("product_id", $value["product_id"], PDO::PARAM_INT);
+            $stmt->execute();
+
+            $res4 = $stmt->fetch();
+            if ($res4['quantity'] < $res3['quantity']) {
+                echo "44";
+
+                return false;
+            }
+        }
+        $total_pice = 0;
+        foreach ($res3 as $key => $value) {
+            $sql4 = "SELECT * FROM products WHERE id = :product_id";
+
+            $stmt = $pdo->prepare($sql4);
+            $stmt->bindValue("product_id", $value["product_id"], PDO::PARAM_INT);
+            $stmt->execute();
+
+            $res4 = $stmt->fetch();
+
+            $sql5 = "UPDATE products SET quantity = :quantity WHERE id = :product_id";
+
+            $stmt = $pdo->prepare($sql5);
+            $stmt->bindValue(":product_id", $value["product_id"], PDO::PARAM_INT);
+            $stmt->bindValue(":quantity", $res4["quantity"] - $value["quantity"], PDO::PARAM_INT);
+            $stmt->execute();
+
+            $total_pice += ((float)$res4["price"] * $res3["quantity"]);
+        }
+
+        $sql6 = "INSERT INTO orders(user_id, cart_id, total_price) VALUES (:user_id, :cart_id, :total_price)";
+
+        $stmt = $pdo->prepare($sql6);
+        $stmt->bindValue(":user_id", $userId, PDO::PARAM_INT);
+        $stmt->bindValue(":cart_id", $cartId, PDO::PARAM_INT);
+        $stmt->bindValue(":total_price", $total_pice, PDO::PARAM_STR);
+        $stmt->execute();
+
+        $res6 = $pdo->lastInsertId();
+
+        return $res6;
     }
 }
